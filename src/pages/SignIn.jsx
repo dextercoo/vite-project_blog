ae6c24fb8c06d362a7a23471dce778230dc49006
dextercoo <azharkhan10000000000@gmail.com>
2024-02-07 22:45:57 +0530

    Create header  component

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function SignIn() {
   return (
     <div>
-      <h1>singn-in</h1>
+      sign-in
     </div>
   )
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function SignUp() {
   return (
     <div>
-      <h1>singn-up</h1>
+      singn-up
     </div>
   )
 }
